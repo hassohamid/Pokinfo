8c3e414b098c9788257b5f282eb05f67a05c319d
hassohamid <hassohamid@hotmail.com>
2024-11-18 23:30:15 +0100

    setTimeout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import "./App.css";
 import PokemonApplication from "./components/PokemonApplication";
 import Pokemon from "./components/Pokemon";
+import { useState } from "react";
 
 function App() {
+  // implementing state to be able to show my PokemonApp component in the DOM onclick
+  const [isVisible, setIsVisible] = useState(true);
+  //  adding a small timer after user presses the btn for a nicer touch
+  const [isLoading, setIsLoading] = useState(false);
+
+  function setVisibility() {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+      setIsVisible(false);
+    }, 2000);
+  }
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="logoposition">
+        <img src="/infokemon.png" alt="Website Logo" className="h-28" />
+      </div>
+      {isVisible ? (
+        <div className="flex flex-col justify-center items-center w-[100vw] h-[70vh]">
+          <img
+            className="animate-bounce w-14 h-14"
+            src="/pokeball.png"
+            alt="Pokemon Ball"
+          />
+          <div className="startbtncontainer">
+            <button onClick={setVisibility}>
+              {isLoading ? "Joruney starting..." : "Start Your Journey"}
+            </button>
+          </div>
+        </div>
+      ) : (
+        <PokemonApplication />
+      )}
     </>
   );
 }
--- src/components/PokemonApplication.jsx
+++ src/components/PokemonApplication.jsx
@@ -1,3 +1,28 @@
-function PokemonApplication() {}
+import React, { useState, useEffect } from "react";
+
+function PokemonApplication() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState("");
+  const [pokemonData, setPokemonData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => response.json())
+      .then((data) => setPokemonList(data.results))
+      .catch((error) => console.error("Error fetching Pokemon list:", error));
+  }, []);
+
+  const fetchPokemonData = () => {
+    if (selectedPokemon) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => setPokemonData(data))
+        .catch((error) => console.error("Error fetching Pokemon data:", error));
+    }
+  };
+
+  return <div className="container"></div>;
+}
 
 export default PokemonApplication;
